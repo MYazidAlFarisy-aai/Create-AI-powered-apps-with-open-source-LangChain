b83f3e1aeda6358e3005b99f94ef8f2151b33ed5
M Yazid Al Farisy <136563673+MYazidAlFarisy-aai@users.noreply.github.com>
2024-03-06 07:13:50 +0000

    6 projek membuat AI menggunakan LangChain

---
+++ latihan_langkah.py
@@ -0,0 +1,22 @@
+from langchain.prompts import PromptTemplate
+from langchain_openai import ChatOpenAI
+import gradio as gr
+
+# initialize the models
+openai = ChatOpenAI(
+    model_name="gpt-3.5-turbo",
+    openai_api_key="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"
+)
+
+def chatbot(input_pertanyaan):
+    # defining a template
+    template = """pertanyaan: {pertanyaan}
+    Tolong berikan langkah langkahnya:
+    """
+    prompt = PromptTemplate(template=template, input_variables=["pertanyaan"])
+    formated_prompt =prompt.format(pertanyaan=str(input_pertanyaan))
+    return openai.invoke(formated_prompt).content
+
+demo = gr.Interface(fn=chatbot, inputs="text", outputs="text")
+
+demo.launch(share=True)
